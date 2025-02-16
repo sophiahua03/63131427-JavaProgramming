7d857ca96015c63c48a722b7cbea26e6bf914066
sophiahua03 <tien.hnt.63cntt@ntu.edu.vn>
2025-02-16 20:00:27 +0700

    Sửa code

---
+++ ViDuHuongDoiTuong/src/CT.java
@@ -0,0 +1,23 @@
+
+public class CT {
+
+	public static void main(String[] args) {
+		// Tạo ra 2 sản phẩm (hardcode)
+		
+		SanPham sp1 = new SanPham();
+		SanPham sp2 = new SanPham(2, "Bánh Chuối", "Đồ ăn nhanh", "bc.jpg");
+		sp1.setMaSP(1);
+		sp1.setTenSP(" Bia SG");
+		sp1.setLoaiSP(" Đồ uống có cồn");
+		sp1.setAnhSP(" sg.png");
+		
+		// in ra màn hình thông tin 2 sp
+		String thongtinSP1 = sp1.toString();
+		String thongtinSP2 = "Mã SP2 là " + sp2.getMaSP();
+			   thongtinSP2 +="\nTên SP2 là " + sp2.getTenSP();
+			   
+		System.out.println(thongtinSP1);
+		System.out.println(thongtinSP2);
+	}
+
+}
--- ViDuHuongDoiTuong/src/SanPham.java
+++ ViDuHuongDoiTuong/src/SanPham.java
@@ -1,10 +1,4 @@
-/**
- * 
- */
-
-/**
- * 
- */
+ 
 public class SanPham {
 	//Định nghĩa các thuộc tính
 	int maSP;
@@ -12,44 +6,56 @@ public class SanPham {
 	String loaiSP;
 	String anhSP;
 	
-	public SanPham()
-	{
-		// Khởi tạo giá trị ban đầu cho các thuộc tính nếu có ở đây
+	public SanPham() {
+		super();
 	}
-	
-	//public SanPham(int _maSP, String _tenSP, String _loaiSP, String _anhSP)
-	//{
-		// Khởi tạo giá trị ban đầu cho các thuộc tính nếu có ở đây
-		//maSP = _maSP;
-		//tenSP = _tenSP;
-		//loaiSP = _loaiSP;
-		//anhSP = _anhSP;
-	//}
-	
-	public SanPham(int maSP, String tenSP, String loaiSP, String anhSP)
-	{
-		// Khởi tạo giá trị ban đầu cho các thuộc tính nếu có ở đây
+
+	public SanPham(int maSP, String tenSP, String loaiSP, String anhSP) {
+		super();
 		this.maSP = maSP;
 		this.tenSP = tenSP;
 		this.loaiSP = loaiSP;
 		this.anhSP = anhSP;
 	}
-	
-	//Các getter, setter
-	public int getMaSP()
-	{
+
+
+
+	public int getMaSP() {
 		return maSP;
 	}
-	
-	public void setMaSP(int maSP)
-	{
-		this.maSP = maSP;
-		
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	public void setMaSP(int maSP) {
+		this.maSP = maSP;
+	}
+
+	public String getTenSP() {
+		return tenSP;
+	}
+
+	public void setTenSP(String tenSP) {
+		this.tenSP = tenSP;
+	}
+
+	public String getLoaiSP() {
+		return loaiSP;
+	}
+
+	public void setLoaiSP(String loaiSP) {
+		this.loaiSP = loaiSP;
+	}
+
+	public String getAnhSP() {
+		return anhSP;
+	}
+
+	public void setAnhSP(String anhSP) {
+		this.anhSP = anhSP;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "[Mã SP=" + maSP + ", Tên SP=" + tenSP + ", Loại SP=" + loaiSP + "]";
 	}
 
 }
