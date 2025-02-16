f8e14d8e936df040dd7a7905024c0195067e4b61
sophiahua03 <tien.hnt.63cntt@ntu.edu.vn>
2025-02-16 15:36:05 +0700

    Cập nhật code
    Sửa và thêm một dòng mới

--- HelloWorld/src/XinChao.java
+++ HelloWorld/src/XinChao.java
@@ -13,6 +13,7 @@ public class XinChao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Lần đầu học Java");
+		System.out.println("Thêm một dòng mới và sửa lại");
 	}
 
 }
